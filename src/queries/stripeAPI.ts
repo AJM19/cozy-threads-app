c319b8bb4b08074dd1f081ecf6e720f96bbfd5b8
AJ Milbauer <ajmilbauer@AJs-MacBook-Pro.local>
2025-02-10 18:46:35 -0500

    build fix

--- src/queries/stripeAPI.ts
+++ src/queries/stripeAPI.ts
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { Product } from "../Carousel";
+import { Product } from "../components/Carousel";
 
 export const stripeAPI = createApi({
   reducerPath: "stripeAPI",
